3eeec4c2a37a983bf1bdc21af4e39a149fd5b17f
Dino Andrighetto <Deeno@dinos-macbook-pro.local>
2017-10-14 15:38:36 -0700

    Initial Commit

---
+++ FocusTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FocusTime
+//
+//  Created by Dino Andrighetto on 10/14/17.
+//  Copyright © 2017 Dino Andrighetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
